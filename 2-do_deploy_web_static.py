457a229697526dbf06e2c10441d9c84ff7476586
Kad-19 <kidusasebe1919@gmail.com>
2024-05-07 02:18:38 -0700

    ip updated

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.89.109.87', '100.25.190.21']
+env.hosts = ['52.201.220.251', '54.237.95.128']
 
 
 def do_deploy(archive_path):
