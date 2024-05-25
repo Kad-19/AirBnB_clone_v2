70ae97ac131201137a4f207722d538ad9712cb1f
Kad-19 <kidusasebe1919@gmail.com>
2024-05-25 14:10:55 +0000

    pep8

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,11 +12,12 @@ def home():
     """home screen"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Wolcome to hbnb"""
     return "HBNB"
-	
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
