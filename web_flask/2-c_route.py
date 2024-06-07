6d799930834224026c6a8c7e78b79f214d3893f1
Kad-19 <kidusasebe1919@gmail.com>
2024-06-07 11:14:02 +0000

    display string and numbers

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Starts a flask application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home():
+    """home screen"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Wolcome to hbnb"""
+    return "HBNB"
+
+@app.route('/c/<text>')
+def c_then_params(text):
+    """C is fun"""
+    text_mod = text.replace('_', ' ')
+    return "C {}".format(text_mod)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+    Starts a flask application
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home():
+    """home screen"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Wolcome to hbnb"""
+    return "HBNB"
+
+@app.route('/c/<text>')
+def c_then_params(text):
+    """C is fun"""
+    text_mod = text.replace('_', ' ')
+    return "C {}".format(text_mod)
+
+@app.route('/python', defaults={'text':'is_cool'})
+@app.route('/python/<text>')
+def python_then_params(text):
+    """Python is cool"""
+    text_mod = text.replace('_', ' ')
+    return "Python {}".format(text_mod)
+
+@app.route('/number/<int:n>')
+def number(n):
+    """is it a number"""
+    return "{} is a number".format(n)
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """Number template"""
+    return render_template('5-number.html', number=n)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
